a7f54b295b3289cc3f50105c540985a89e460360
thanhviet3005 <thanhviet3005@gmail.com>
2020-12-09 10:39:48 +0700

    hi

---
+++ _s6_cascading_style_sheet/bt/MucLucTruyenTranh.js
@@ -0,0 +1,13 @@
+function get1() {
+    let temp1 = "Public Enemies hy vọng sẽ lặp lại được những thành công của bộ phim “The Departed”";
+    document.getElementById("show1").innerHTML = temp1;
+}
+function get2() {
+    document.getElementById("show2").innerHTML="Public Enemies hy vọng sẽ lặp lại được những thành công của bộ phim “The Departed”";
+}
+function get3() {
+    document.getElementById("show3").innerHTML="Public Enemies hy vọng sẽ lặp lại được những thành công của bộ phim “The Departed”";
+}
+function get4() {
+    document.getElementById("show4").innerHTML="Public Enemies hy vọng sẽ lặp lại được những thành công của bộ phim “The Departed”";
+}
